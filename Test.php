2134d7d0649b8aae68478d0c1c7140a97c4b4a67
1513072 <a>
2016-04-19 15:22:27 +0100

    CMM007

--- Test.php
+++ Test.php
@@ -8,8 +8,8 @@
 
 <form action="insert.php" method="post">
 
-    <input type="text" name="firstname" placeholder="First Name">
-    <input type="text" name="lastname" placeholder="Last Name">
+    <input type="text" name="firstname" placeholder="First Name"><br>
+    <input type="text" name="lastname" placeholder="Last Name"><br>
     <input type="submit" name="Submit">
 
 </form>
