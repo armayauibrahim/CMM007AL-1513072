38abb9c332ee8618729e17ffb3b7ad927bfd1f6b
1513072 <a>
2016-04-19 15:17:51 +0100

    CMM007

--- Test.php
+++ Test.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+    <title>My Test Page</title>
+</head>
+<body>
+
+<form action="insert.php" method="post">
+
+    <input type="text" name="firstname" placeholder="First Name">
+    <input type="text" name="lastname" placeholder="Last Name">
+    <input type="submit" name="Submit">
+
+</form>
+
+</body>
+</html>
+
+
+
+
 <?php
 echo "My test page is working.";
 ?>
